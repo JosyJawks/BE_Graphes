2a859e8a55c46b8ffaf094de6c6897d6b3c70cd7
Ramouche Josua <ramouche@insa-20154>
2023-04-05 16:42:00 +0200

    classe Label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,5 +1,41 @@
 package org.insa.graphs.algorithm.shortestpath;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
 
 public class Label {
     
+    protected Node currentNode;
+
+    protected boolean mark;
+
+    protected float realCost;
+
+    protected Arc father;
+
+//Getters
+
+public Node getcurrentNode(){
+    return this.currentNode;
+}
+
+public boolean getMark(){
+    return this.mark;
+}
+
+public float getrealCost(){
+    return this.realCost;
+}
+
+public Arc getFather(){
+    return this.father;
+}
+
+//Methods
+public float getCost(){
+    float cost;
+    cost = this.realCost;
+    return cost;
+}
+
+
 }
